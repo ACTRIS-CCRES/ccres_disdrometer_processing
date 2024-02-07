b5b5f357c3d6c6e1892e92e87352c0fcb4aec1ab
Marc-Antoine Drouin <marc-antoine.drouin@lmd.ipsl.fr>
2024-02-07 09:18:52 +0100

    rename test sub dir for uniformity

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -2,10 +2,9 @@
 
 from pathlib import Path
 
-import pytest
 from click.testing import CliRunner
 
-from ccres_disdrometer_processing.ccres_disdrometer_processing.cli import cli
+from ccres_disdrometer_processing.cli import cli
 
 MAIN_DIR = Path(__file__).parent.parent.parent
 TEST_DIR = MAIN_DIR / "tests"
@@ -14,40 +13,30 @@ TEST_OUT_DIR = TEST_DIR / "data/outputs"
 EXE = MAIN_DIR / "ccres_disdrometer_processing/cli/cli2.py"
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface_help():
+def test_preprocess_interface_help() -> None:
     """Test the Help argument of CLI."""
     runner = CliRunner()
-    help_result = runner.invoke(cli.main, ["--help"])
+    help_result = runner.invoke(cli.preprocess, ["--help"])
     assert help_result.exit_code == 0
     assert "Show this message and exit." in help_result.output
 
 
-def test_command_line_interface():
+def test_preprocess_interface() -> None:
     """Test the CLI."""
+<<<<<<< HEAD:tests/test_cli/test_cli.py
     disdro_file = f"{TEST_INPUT}/20210202_palaiseau_parsivel.nc"
+=======
+    # disdro_type = "parsivel-cloudnet"
+    disdro_file = f"{TEST_INPUT}/20210202_palaiseau_parsivel.nc"
+    # ws_type = "sirta-cloudnet"
+>>>>>>> 4dc8483 (rename test sub dir for uniformity):tests/cli/test_cli.py
     ws_file = f"{TEST_INPUT}/20210202_palaiseau_weather-station.nc"
     radar_file = f"{TEST_INPUT}/20210202_palaiseau_basta.nc"
     config_file = f"{TEST_INPUT}/CONFIG_test.toml"
     output_file = f"{TEST_OUT_DIR}/20210202_palaiseau_preprocessed_v2311.nc"
     runner = CliRunner()
     result = runner.invoke(
-        cli.main,
+        cli.preprocess,
         [
             "--disdro-file",
             disdro_file,
