d20a825c546f62fedc9bac15f3ba9bceaaafda21
Marc-Antoine Drouin <marc-antoine.drouin@lmd.ipsl.fr>
2024-02-16 12:19:23 +0100

    rewrite logger function and tests

--- tests/logger/test_logger.py
+++ tests/logger/test_logger.py
@@ -16,7 +16,7 @@ def test_log_level_from_count():
 
 
 def test_init_logger():
-    init_logger(level=LogLevels.INFO)
-    lgr = logging.getLogger("ccres_disdrometer_processing")
-    assert logging.getLevelName(lgr.handlers[0].level) == "INFO"
-    assert LOG_FORMAT in logging.getLevelName(lgr.handlers[0].formatter._fmt)
+    lgr = init_logger("ccres_disdrometer_processing", LogLevels.INFO)
+
+    assert logging.getLevelName(lgr.root.handlers[0].level) == "INFO"
+    assert LOG_FORMAT in logging.getLevelName(lgr.root.handlers[0].formatter._fmt)
