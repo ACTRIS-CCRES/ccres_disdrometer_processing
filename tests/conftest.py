ae9e339ce95a223efe7d978f088b429e03e7229d
Marc-Antoine Drouin <marc-antoine.drouin@lmd.ipsl.fr>
2024-02-16 17:33:58 +0100

    feat: use pytest to test preprocessing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,54 +1,68 @@
 """Fixtures for pytest."""
+
+from pathlib import Path
+
 import pytest
-import requests
-
-CLOUDNET_API_URL = "https://cloudnet.fmi.fi/api/"
-CLOUDNET_API_RAW_URL = "https://cloudnet.fmi.fi/api/raw-files/"
-
-TEST_DATA = [
-    {
-        "site": "palaiseau",
-        "date": "2021-02-02",
-        "radar": "basta",
-        "radar-pid": "https://hdl.handle.net/21.12132/3.643b7b5b43814e6f",
-        "disdro": "parsivel",
-        "disdro-pid": "https://hdl.handle.net/21.12132/3.7e13f3f243854ae8",
-        "meteo-availlable": True,
-        "meteo": "weather-station",
-        "meteo-pid": "https://hdl.handle.net/21.12132/3.739041931dac4de5",
-    }
-]
 
 
 @pytest.fixture
 def root_dir(request):
-    return request.config.rootdir
+    path = request.config.rootdir
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+    return Path(path)
 
 
 @pytest.fixture
 def data_dir(root_dir):
-    return root_dir / "tests" / "data"
+    path = root_dir / "tests" / "data"
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+    return path
 
 
 @pytest.fixture
 def data_input_dir(data_dir):
-    return data_dir / "inputs"
+    path = data_dir / "inputs"
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+    return path
 
 
 @pytest.fixture
 def data_conf_dir(data_dir):
-    return data_dir / "conf"
+    path: Path = data_dir / "conf"
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+    return path
 
 
 @pytest.fixture
 def data_out_dir(data_dir):
-    return data_dir / "outputs"
+    path: Path = data_dir / "outputs"
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+    return path
 
 
-def pytest_sessionstart() -> None:
-    """Will be call before running tests."""
-    # Check if the Cloudnet API is available
-    try:
-        requests.get(CLOUDNET_API_URL)
-    except requests.exceptions.ConnectionError:
-        pytest.exit("Cloudnet API is not available")
+@pytest.fixture
+def test_data_preprocessing():
+    return [
+        {
+            "site": "palaiseau",
+            "date": "2021-02-02",
+            "radar": "basta",
+            "radar-pid": "https://hdl.handle.net/21.12132/3.643b7b5b43814e6f",
+            "disdro": "parsivel",
+            "disdro-pid": "https://hdl.handle.net/21.12132/3.7e13f3f243854ae8",
+            "meteo-available": True,
+            "meteo": "weather-station",
+            "meteo-pid": "https://hdl.handle.net/21.12132/3.739041931dac4de5",
+            "config_file": "config_palaiseau_basta-parsivel-ws.toml",
+        }
+    ]
--- tests/run/test_preprocess.py
+++ tests/run/test_preprocess.py
@@ -1,7 +1,70 @@
 """Tests preprocessing."""
-from pathlib import Path
+
+import pytest
+from click.testing import CliRunner
+
+from ccres_disdrometer_processing.cli import cli
+from tests import utils
 
 
-def test_run(root_dir: Path) -> None:
-    print("path ", root_dir)
-    print("type", type(root_dir))
+@pytest.fixture
+def test_case(test_data_preprocessing):
+    yield from test_data_preprocessing
+
+
+def test_run(test_case, data_input_dir, data_conf_dir, data_out_dir) -> None:
+    """Test the preprocessing for a specific test case."""
+    site = test_case["site"]
+    date = test_case["date"]
+    radar = test_case["radar"]
+    disdro = test_case["disdro"]
+    has_meteo = test_case["meteo-available"]
+    meteo = test_case["meteo"]
+    conf = test_case["config_file"]
+
+    # get the data if needed
+    # ---------------------------------------------------------------------------------
+    # radar
+    radar_file = utils.get_file_from_cloudnet(site, date, radar, data_input_dir)
+    # disdro
+    disdro_file = utils.get_file_from_cloudnet(site, date, disdro, data_input_dir)
+    # meteo
+    if test_case["meteo-available"]:
+        meteo_file = utils.get_file_from_cloudnet(site, date, meteo, data_input_dir)
+
+    # other parameters
+    # ---------------------------------------------------------------------------------
+    # conf
+    conf = data_conf_dir / conf
+    # output
+    output_file = data_out_dir / f"{site}_{date}_preprocessed.nc"
+
+    # run the preprocessing
+    # ---------------------------------------------------------------------------------
+    # required args
+    args = [
+        "--disdro-file",
+        str(disdro_file),
+        "--radar-file",
+        str(radar_file),
+        "--config-file",
+        str(conf),
+    ]
+    # add meteo if available
+    if has_meteo:
+        args += [
+            "--ws-file",
+            str(meteo_file),
+        ]
+
+    args += [str(output_file)]
+
+    print(args)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli.preprocess,
+        args,
+    )
+
+    assert result.exit_code == 0
