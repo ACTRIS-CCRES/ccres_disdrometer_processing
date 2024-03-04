ad3da50dd09619b4c88442abebbb33f64843549d
ygrit <ygrit@ipsl.fr>
2024-03-04 15:36:12 +0100

    some improvement

--- ccres_disdrometer_processing/preprocessed_file2processed.py
+++ ccres_disdrometer_processing/preprocessed_file2processed.py
@@ -3,6 +3,7 @@
 Input : Daily preprocessed files at days D and D-1
 Output : Daily processed file for day D
 """
+
 import logging
 
 import numpy as np
@@ -77,53 +78,165 @@ def rain_event_selection(ds, conf):
 def extract_dcr_data(ds, conf):
     # Extract DCR Ze at 3/4 relevant gates, extract DD Ze, compute Delta Ze
     # Get Doppler velocity at relevant gates, compute avg disdrometer fall speed(t)
+    Ze_ds = xr.Dataset(coords=dict(time=(["time"], ds.time.data)))
 
     # DCR data extract
     ranges_to_keep = conf["plot_parameters"]["DCR_PLOTTED_RANGES"]
-    Zdcr = ds["Zdcr"].sel({"range": ranges_to_keep}, method="nearest")
-    DVdcr = ds["DVdcr"].sel({"range": ranges_to_keep}, method="nearest")
+    Ze_ds["Zdcr"] = ds["Zdcr"].sel({"range": ranges_to_keep}, method="nearest")
+    Ze_ds["DVdcr"] = ds["DVdcr"].sel({"range": ranges_to_keep}, method="nearest")
     # Disdrometer data extract
-    Zdd = ds["Zdlog_vfov_modv_tm"]
-    fallspeed_dd = np.nansum(
+    Ze_ds["Zdd"] = ds["Zdlog_vfov_modv_tm"]
+    Ze_ds["fallspeed_dd"] = np.nansum(
         np.nansum(ds["psd"].values, axis=2) * ds["measV"].values, axis=1
     )
     # Delta Ze
-    Delta_Z = Zdcr - Zdd
+    Ze_ds["Delta_Z"] = Ze_ds["Zdcr"] - Ze_ds["Zdd"]
 
-    return Zdcr, DVdcr, Zdd, fallspeed_dd, Delta_Z
+    return Ze_ds
 
 
 def compute_quality_checks_weather(ds, conf, start, end):
+    qc_ds = xr.Dataset(coords=dict(time=(["time"], ds.time.data)))
+
     # flag the timesteps belonging to an event
-    flag_event = 0
+    qc_ds["flag_event"] = xr.DataArray(
+        data=np.full(len(ds.time), False, dtype=bool), dims=["time"]
+    )
     # do a column for rain accumulation since last beginning of an event
-    flag_rain_acc = 0
-    # Weather quality flags
-    QF_ta = ds["ta"].values > conf["thresholds"]["MIN_TEMP"]
-    QF_ws = ds["ws"].values > conf["thresholds"]["MAX_WS"]
+    qc_ds["ams_cum_since_event_begin"] = xr.DataArray(
+        np.zeros(len(ds.time)), dim="time"
+    )
+    qc_ds["dd_cum_since_event_begin"] = xr.DataArray(np.zeros(len(ds.time)), dim="time")
+    for s, e in zip(start, end):
+        qc_ds["flag_event"].sel(time=slice(s, e)).values = True
+        qc_ds["ams_cum_since_event_begin"].sel(time=slice(s, e)).values = (
+            1 / 60 * np.cumsum(ds["ams_pr"].sel(time=slice(s, e)).values)
+        )
+        qc_ds["dd_cum_since_event_begin"].sel(time=slice(s, e)).values = (
+            1 / 60 * np.cumsum(ds["dd_pr"].sel(time=slice(s, e)).values)
+        )
+
+    # Flag for condition (rainfall_amount > N mm)
+    qc_ds["QF_ams_rainfall_amount"] = xr.DataArray(
+        ds["ams_cum_since_event_begin"] >= conf["thresholds"]["MIN_RAINFALL_AMOUNT"],
+        dim="time",
+    )
+
+    # Temperature QC
+    qc_ds["QC_ta"] = xr.DataArray(
+        ds["ta"].values > conf["thresholds"]["MIN_TEMP"], dim="time"
+    )
+    # Wind speed and direction QCs
+    qc_ds["QC_ws"] = xr.DataArray(
+        ds["ws"].values > conf["thresholds"]["MAX_WS"], dim="time"
+    )
     main_wind_dir = (conf["instrument_parameters"]["DD_ORIENTATION"] + 90) % 360
     dd_angle = conf["thresholds"]["DD_ANGLE"]
-    QF_wd = (np.abs(ds["wd"] - main_wind_dir) < dd_angle) | (
-        np.abs(ds["wd"] - (main_wind_dir + 180) % 360) < dd_angle
+    qc_ds["QC_wd"] = xr.DataArray(
+        (np.abs(ds["wd"] - main_wind_dir) < dd_angle)
+        | (np.abs(ds["wd"] - (main_wind_dir + 180) % 360) < dd_angle),
+        dim="time",
     )
-    QF_ams_pr = ds["ams_pr"].values < conf["thresholds"]["MAX_RR"]
+    # QC on AMS precipitation rate
+    qc_ds["QC_ams_pr"] = xr.DataArray(
+        data=np.full(len(ds.time), True, dtype=bool), dims=["time"]
+    )
 
-    QF_rg_dd = np.zeros(len(ds.time))
     for s, e in zip(start, end):
-        # extract ds(start to end), compute relative deviation
-        # and compare to the tolerance specified in the conf (?)
-        break
+        time_chunks = np.arange(
+            np.datetime64(s),
+            np.datetime64(e),
+            np.timedelta64(conf["thresholds"]["PR_SAMPLING"], "m"),
+        )
+        for start_time_chunk, stop_time_chunk in zip(time_chunks[:-1], time_chunks[1:]):
+            RR_chunk = (
+                ds["ams_pr"]
+                .sel(
+                    {
+                        "time": slice(
+                            start_time_chunk, stop_time_chunk - np.timedelta64(1, "m")
+                        )
+                    }
+                )
+                .mean()
+            )
 
-    pass
+            print(
+                qc_ds["QC_ams_pr"]
+                .sel(
+                    {
+                        "time": slice(
+                            start_time_chunk, stop_time_chunk - np.timedelta64(1, "m")
+                        )
+                    }
+                )
+                .values.shape
+            )
+
+            qc_ds["QC_ams_pr"].sel(
+                {
+                    "time": slice(
+                        start_time_chunk, stop_time_chunk - np.timedelta64(1, "m")
+                    )
+                }
+            ).values = np.tile(
+                (RR_chunk <= conf["thresholds"]["MAX_RR"]),
+                conf["thresholds"]["PR_SAMPLING"],
+            )
+
+    # Check agreement between rain gauge and disdrometer rain measurements
+    # extract ds(start to end), compute relative deviation and compare to conf tolerance
+    qc_ds["QF_rg_dd"] = xr.DataArray(np.zeros(len(ds.time)), dim="time")
+    for s, e in zip(start, end):
+        event_mask = np.where((ds.time.values >= s) | (ds.time.values <= e))[0]
+        qc_ds["QF_rg_dd"].isel({"time": event_mask}).values = (
+            np.abs(
+                ds["dd_cum_since_event_begin"].isel({"time": event_mask}).values
+                - ds["ams_cum_since_event_begin"].isel({"time": event_mask}).values
+            )
+            / ds["ams_cum_since_event_begin"].isel({"time": event_mask}).values
+            < conf["thresholds"]["DD_RG_MAX_PR_ACC_RATIO"]
+        )
+
+    # QC relationship v(dsd)
+    qc_ds["QC_vdsd_t"] = xr.DataArray(
+        data=np.full(len(ds.time), False, dtype=bool), dims=["time"]
+    )
+
+    vth_disdro = (np.nansum(ds["psd"].values, axis=2) @ ds["modV"].values) / np.nansum(
+        ds["psd"].values, axis=(1, 2)
+    )  # average fall speed weighed by (num_drops_per_time_and_diameter)
+    vobs_disdro = (
+        np.nansum(ds["psd"].values, axis=1) @ ds["speed_classes"].values
+    ) / np.nansum(ds["psd"].values, axis=(1, 2))
+    ratio_vdisdro_vth = vobs_disdro / vth_disdro
+
+    qc_ds["QC_vdsd_t"] = xr.DataArray(
+        data=(np.abs(ratio_vdisdro_vth - 1) <= 0.3), dim="time"
+    )
+
+    return qc_ds
 
 
 def compute_quality_checks_noweather(ds, conf):
-    pass
+    QF_dd_pr = ds["ams_pr"].values < conf["thresholds"]["MAX_RR"]
+    return QF_dd_pr
 
 
-def compute_todays_events_stats(ds, conf):
-    pass
+def compute_todays_events_stats(ds, conf, start, end):
+    event_stats_ds = xr.Dataset(coords=dict(time=(["time"], ds.time.data)))
+    return event_stats_ds
 
 
 def store_outputs(ds, conf):
     pass
+
+
+if __name__ == "__main__":
+    yesterday = "/homedata/ygrit/"
+    today = 0
+    tomorrow = 0
+    conf = 0
+    ds = merge_preprocessed_data(yesterday, today, tomorrow)
+    x = rain_event_selection(ds, conf)
+    print(x)
