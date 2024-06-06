864aca797a835ef2f9c7424c15deafb38f4e6fb3
ygrit <ygrit@ipsl.fr>
2024-06-06 16:11:30 +0200

    implemented a small test for the --no-meteo option in the process cli

--- tests/run/test_03_process.py
+++ tests/run/test_03_process.py
@@ -11,7 +11,7 @@ from tests import utils
 def test_run_one_day(
     test_data_preprocessing, data_input_dir, data_conf_dir, data_out_dir
 ) -> None:
-    """Test the preprocessing for a specific test case."""
+    """Test the processing for a specific test case."""
     conf = test_data_preprocessing["config_file"]
     today_file = data_out_dir / test_data_preprocessing["output"]["preprocess"]
     output_file = data_out_dir / test_data_preprocessing["output"]["process"]
@@ -21,7 +21,7 @@ def test_run_one_day(
     # conf
     conf = data_conf_dir / conf
 
-    # run the preprocessing
+    # run the processing
     # ---------------------------------------------------------------------------------
     # required args
     args = ["--today", str(today_file), "--config-file", str(conf), str(output_file)]
@@ -36,10 +36,52 @@ def test_run_one_day(
     assert result.exit_code == 0
 
 
+def test_run_one_day_no_meteo_cli(
+    test_data_processing_cli_option_no_meteo,
+    data_input_dir,
+    data_conf_dir,
+    data_out_dir,
+) -> None:
+    """Test the processing for a specific test case."""
+    conf = test_data_processing_cli_option_no_meteo["config_file"]
+    today_file = (
+        data_out_dir / test_data_processing_cli_option_no_meteo["output"]["preprocess"]
+    )
+    output_file = (
+        data_out_dir / test_data_processing_cli_option_no_meteo["output"]["process"]
+    )
+
+    # other parameters
+    # ---------------------------------------------------------------------------------
+    # conf
+    conf = data_conf_dir / conf
+
+    # run the processing
+    # ---------------------------------------------------------------------------------
+    # required args
+    args = [
+        "--today",
+        str(today_file),
+        "--config-file",
+        str(conf),
+        "--no-meteo",
+        str(output_file),
+    ]
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli.process,
+        args,
+        catch_exceptions=False,
+    )
+
+    assert result.exit_code == 0
+
+
 def test_run_ndays(
     test_data_processing_ndays, data_input_dir, data_conf_dir, data_out_dir
 ) -> None:
-    """Test the preprocessing for a specific test case."""
+    """Test the processing for a specific test case."""
     site = test_data_processing_ndays["site"]
     dates = test_data_processing_ndays["list_dates"]
     radar = test_data_processing_ndays["radar"]
@@ -67,7 +109,7 @@ def test_run_ndays(
             "preprocess_tmpl"
         ].format(date)  # noqa E501
 
-        # run the preprocessing
+        # run the processing
         # ------------------------------------------------------------------------------
         # required args
         args = [
